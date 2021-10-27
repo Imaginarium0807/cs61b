0cfdf7a17bd643c27881bfa3694b2b9e7dff919b
Elain Hu <besthynlove@gmail.com>
2021-10-27 08:49:39 -0400

    add new

--- src/twoSum.java
+++ src/twoSum.java
@@ -1,6 +1,7 @@
+import java.util.HashMap;
 public class twoSum {
-    public int[] twoSum(int[] nums, int target){
-        Map<Integer, Integer> map = new HashMap<>();
+    public int[] twoSum(int[] nums, int[] target){
+        Map<Integer, Integer> map = new HashMap<int, int>();
     for (int i = 0; i < nums.length; i++) {
         int complement = target - num[i];
         if (map.containsKey(complement)) {
