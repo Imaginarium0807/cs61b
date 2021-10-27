8c890103957bbe7dce92903e1fc5d8f575158b26
Elain Hu <besthynlove@gmail.com>
2021-10-27 08:28:09 -0400

    not fixed

---
+++ src/twoSum.java
@@ -0,0 +1,18 @@
+public class twoSum {
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - num[i];
+        if (map.containsKey(complement)) {
+            return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] n1 = new int[]{2, 7, 11, 15};
+        int[] n2 = new int[]{9};
+        System.out.println(twoSum(n1, n2));
+    }
+}
