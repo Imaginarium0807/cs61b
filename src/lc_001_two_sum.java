3aaf362cac59e88f9c274fc5d8793487a100d55d
Elain Hu <besthynlove@gmail.com>
2021-10-31 21:48:29 -0400

    update

--- src/lc_001_two_sum.java
+++ src/lc_001_two_sum.java
@@ -15,13 +15,34 @@ public class lc_001_two_sum {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+    //Use the keySet() method if you only want the keys, and use the values() method if you only want the values:
+    public static void MapCat() {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
 
+        // Displaying the HashMap
+        System.out.println("Initial Mappings are: " + capitalCities);
+        // Displaying key/value pair
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+
+        // Checking for the key_element "USA"
+        System.out.println(capitalCities.containsKey("USA"));
+        // Display value of key_element "USA"
+        System.out.println(capitalCities.get("USA"));
+    }
 
     public static void main(String[] args){
         int[] intArray = new int[]{2,5,2,6,3};
         int target = 5;
         int[] result = twoSum(intArray, target);
         System.out.println(Arrays.toString(result));
+        // test MapCat
+        MapCat();
     }
 }
 
