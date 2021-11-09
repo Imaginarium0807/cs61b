9a87596d8a7bec41701a5c848d379987ed368200
Elain Hu <besthynlove@gmail.com>
2021-11-09 08:16:50 -0500

    add

--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,6 @@
 public class Dog {
-    public static int weightInPounds; ///Instance variable
-    public static int Dog(int startingWeight){ /// Constructor
-        weightInPounds = startingWeight;
-        return weightInPounds;
-    }
-    public static String makeNoise(int weightInPounds){ ///non-static method, instance method
+
+    public static void makeNoise(int weightInPounds){
         if (weightInPounds < 10) {
             System.out.println("small dog");
         } else if (weightInPounds < 30) {
@@ -12,11 +8,10 @@ public class Dog {
         } else {
             System.out.println("Good dog!");
         }
-        return "test";
     }
     public static void main(String[] args){
         int weightInPounds = 12;
-        System.out.println(Dog(weightInPounds));
-        System.out.println(makeNoise(weightInPounds));
+
+        makeNoise(weightInPounds);
     }
 }
