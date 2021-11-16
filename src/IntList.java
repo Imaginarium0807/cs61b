727161f3f8a47f60a2909a59dc57bf3eeca2dac3
Elain Hu <besthynlove@gmail.com>
2021-11-16 08:24:24 -0500

    add

--- src/IntList.java
+++ src/IntList.java
@@ -11,15 +11,23 @@ public class IntList {
         first = f;
         rest = r;
     }
+    /** Retrun the size of the list using recursion!*/
+    public int size(){
+        if (rest == null){
+            return 1;
+        }
+        return 1 + this.rest.size();
 
+    }
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
 
 
         L = new IntList(10, L);
         L = new IntList(5, L);
-        System.out.println(L.toString());
-        Stream.of(L.toString()).forEach(System.out::println);
+        //System.out.println(L.toString());
+        //Stream.of(L.toString()).forEach(System.out::println);
+        System.out.println(L.size());
 
     }
 
