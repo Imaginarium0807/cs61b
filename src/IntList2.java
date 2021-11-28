3301eb6e36cdc0bc1b633e66497830dcae945f62
Elain Hu <besthynlove@gmail.com>
2021-11-28 17:18:51 -0500

    test

--- src/IntList2.java
+++ src/IntList2.java
@@ -7,16 +7,27 @@ import java.util.stream.Stream;
 public class IntList2 {
     public int first_number;
     public IntList rest_is_list;
+    public int numNew = 8;
 
     public IntList2() {
 
     }
 
+    public static void myMethod1(){
+        System.out.println("Hellow Mario");
+    }
+
     public static void main(String[] args) {
+        // you can access attributes by creating an object of the class, and by using the dot L.numNew
+        // create an object called L and print the value of x
         IntList2 L = new IntList2();
         L.first_number = 100;
         L.rest_is_list = null;
         System.out.println(L.first_number);
+        System.out.println(L.numNew);
+
+        myMethod1();
+
     }
 
 }
