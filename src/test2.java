5604b7a8e8f70b164ce81ff79332330d5bdeef6f
Elain Hu <besthynlove@gmail.com>
2021-12-12 12:07:19 -0500

    commit

---
+++ src/test2.java
@@ -0,0 +1,12 @@
+public class test2 {
+    public static void makeNoise(){
+        int a = 5;
+        int b;
+        b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static void main(String[] args){
+        makeNoise();
+    }
+}
