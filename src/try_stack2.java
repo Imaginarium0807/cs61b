695a7346289a9c3c128060dc1caacc2d05b7b33e
Elain Hu <besthynlove@gmail.com>
2021-12-13 00:23:27 -0500

    -m

---
+++ src/try_stack2.java
@@ -0,0 +1,21 @@
+import java.util.*; //importing the stack class
+class MyClass2{
+    public static void main (String[] args)
+    {
+        // Creating a Stack
+        Stack<String> run_stack = new Stack<>();
+        run_stack.push("first");
+        System.out.println(run_stack);
+        run_stack.push("second");
+        System.out.println(run_stack);
+        run_stack.push("third");
+        System.out.println(run_stack);
+
+        run_stack.pop();
+        System.out.println(run_stack);
+        run_stack.pop();
+        System.out.println(run_stack);
+        run_stack.pop();
+        System.out.println(run_stack);
+    }
+}
