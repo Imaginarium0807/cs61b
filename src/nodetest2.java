5c09743b006dbcde1d306b72e7c0c11924b5fb31
Elain Hu <besthynlove@gmail.com>
2022-03-27 22:12:06 -0400

    m

---
+++ src/nodetest2.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class nodetest2 {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        l1.add(4);
+
+        System.out.println(l1);
+
+        LinkedList<Integer> l2 = new LinkedList<Integer>();
+        l2.add(1);
+        l2.add(3);
+        l2.add(4);
+
+        System.out.println(l2);
+
+
+    }
+}
